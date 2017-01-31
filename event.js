04275535b8d169a7887126ec908f6f58bb82a190
darshana825 <darshana@eight25media.com>
2017-01-31 18:10:03 +0530

    developed events highlighting

--- event.js
+++ event.js
@@ -26,7 +26,6 @@
 })();
 
 (function (angular) {
-
     var app = angular.module('app');
 
     app.directive('calendar', ['$compile', function ($compile) {
@@ -37,19 +36,23 @@
             },
             templateUrl: 'calendar.html',
             link: function (scope, elem, attr) {
+                var xx = new Date('Wed Feb 01 2017 12:37:15 GMT+0530 (+0530)');
+               // console.log(xx.toISOString());
+
+
                 var date = new Date();
                 scope.calendar = generateCalendar(date.getMonth(), date.getFullYear());
-                renderCalendar(elem, scope.calendar.html);
+                renderHtml(elem, scope.calendar.html);
 
                 scope.prevMonth = function () {
                     var currMonth = scope.calendar.meta.currMonth;
 
                     if (currMonth == 0) {
                         scope.calendar = generateCalendar(11, scope.calendar.meta.year - 1);
-                        renderCalendar(elem, scope.calendar.html);
+                        renderHtml(elem, scope.calendar.html);
                     } else {
                         scope.calendar = generateCalendar(scope.calendar.meta.currMonth - 1, scope.calendar.meta.year);
-                        renderCalendar(elem, scope.calendar.html);
+                        renderHtml(elem, scope.calendar.html);
                     }
                 };
 
@@ -58,12 +61,141 @@
 
                     if (currMonth == 11) {
                         scope.calendar = generateCalendar(0, scope.calendar.meta.year + 1);
-                        renderCalendar(elem, scope.calendar.html);
+                        renderHtml(elem, scope.calendar.html);
                     } else {
                         scope.calendar = generateCalendar(scope.calendar.meta.currMonth + 1, scope.calendar.meta.year);
-                        renderCalendar(elem, scope.calendar.html);
+                        renderHtml(elem, scope.calendar.html);
                     }
                 };
+
+                /**
+                 * go to events in selected date
+                 * @param dayIndex - index of the day in 'oMeta.calendar' object
+                 * **/
+                scope.goToEvents = function (dayIndex) {
+                    var oEventsView = generateEventsView(scope.calendar);
+                    renderHtml(elem, oEventsView);
+                };
+
+
+                /**
+                 * events in selected date
+                 * @param oDay - day object that contains events array
+                 * **/
+                function generateEventsView(oCalendar) {
+                    /*var oDate = new Date(oDay.date);
+                     var monthName = oDate.getMonthName(oDate.getMonth());*/
+
+                    console.log(oCalendar);
+
+                    var timeRange = getTimeRange();
+
+                    var weeksHtml = generateMonthByWeek(oCalendar.meta.calendar);
+
+                    var rangeHtml = '<ul>'
+                    for (var i = 0; i < timeRange.length; i++) {
+                        rangeHtml += '<li>' + timeRange[i] + '</li>';
+                    }
+                    rangeHtml += '</ul>';
+
+                    /** <container> **/
+                    var view = "<div class='event-view-container'>";
+                    /** <header> **/
+                    view += "<div class='header'>";
+                    view += "<h3><button><<</button><button>>></button></h3>";
+                    view += "</div>";
+                    /** </header> **/
+
+                    /** <body> **/
+                    view += "<div class='body'>";
+
+                    view += "<div class='time-container'>";
+                    view += "<h4>TIME</h4>";
+                    view += rangeHtml;
+                    view += "</div>";
+
+                    view += "<div class='grid-container'>";
+                    view += weeksHtml;
+                    view += "</div>";
+
+                    view += "</div>";
+
+                    /** </body> **/
+
+                    view += "</div>";
+                    /** </container> **/
+
+                    return view;
+                }
+
+                function generateMonthByWeek(oMonth) {
+                    var weeks = '<ul>';
+
+                    for (var i = 0; i < oMonth.length; i++) {
+                        var oDate = new Date(oMonth[i].date);
+
+                        var weekDayIndex = oDate.getDay();
+
+                        var eventsInDay = oMonth[i].events;
+
+                        var slots = '';
+
+                        for (var x = 1; x < 46; x++) {
+                            var inEventClass = '';
+
+                            if(oMonth[i].hasEvents){
+                                for (var z = 0; z < eventsInDay.length; z++) {
+                                    var start = new Date(eventsInDay[z].start);
+                                    var end = new Date(eventsInDay[z].end);
+                                }
+
+                                if (start.toDateString() == oDate.toDateString()) {
+
+                                    if (x >= start.getHours() && x <= end.getHours()) {
+                                        inEventClass = 'highlight';
+                                    } else {
+                                        inEventClass = '';
+                                    }
+                                }
+                            }
+
+                            slots += '<div class="line-wrapper"><span class="line ' + inEventClass + '"></span></div>';
+                        }
+
+                        weeks += '<li>';
+
+                        weeks += '<h5>' + oDate.getDayName(weekDayIndex) + ' - '+oDate.getDate()+'</h5>';
+
+                        weeks += '<div class="track">';
+                        weeks += slots;
+                        weeks += '</div>';
+
+                        weeks += '</li>';
+                    }
+
+                    weeks += '</ul>';
+
+                    return weeks;
+                }
+
+                function getTimeRange() {
+                    var aRange = ['00:00'];
+                    var slot = {left: 0, right: 0};
+                    for (var i = 0; i < 46; i++) {
+                        if (i % 2 == 0) {
+                            slot.left += 1;
+                            slot.right = '00';
+                        } else {
+                            slot.right = '30'
+                        }
+                        var str = String(slot.left);
+                        if (str.length < 2) {
+                            str = '0' + str;
+                        }
+                        aRange.push(str + ':' + slot.right);
+                    }
+                    return aRange;
+                }
 
                 /**
                  * get event in given date
@@ -93,8 +225,8 @@
                  * @param elem - target element
                  * @param oCalendar - calendar object that contains html
                  * **/
-                function renderCalendar(elem, oCalendar) {
-                    elem.html(oCalendar);
+                function renderHtml(elem, html) {
+                    elem.html(html);
                     $compile(elem.contents())(scope);
                 }
 
@@ -103,8 +235,6 @@
                  * @param monthIndex - index of month as in javascript Date object
                  * **/
                 function generateCalendar(monthIndex, year) {
-                    console.log(scope.events);
-
                     var oDate = new Date();
 
                     var oMeta = {
@@ -128,6 +258,8 @@
                     }
 
                     for (var i = 1; i <= daysInMonth; i++) {
+                        var actualIndex = i - 1;
+
                         if (firstWeekDay > 6) {
                             firstWeekDay = 0;
                             padding += "</tr><tr>";
@@ -135,7 +267,7 @@
 
                         var thisDate = new Date(monthIndex + 1 + '-' + i + '-' + oMeta.year);
 
-                        var dateObj = {date: thisDate.toISOString()};
+                        var dateObj = {index: i, date: thisDate.toISOString()};
                         var eventsInThisDate = getEvents(thisDate.toDateString());
 
                         dateObj = Object.assign(dateObj, eventsInThisDate);
@@ -153,9 +285,9 @@
                         oMeta.calendar.push(dateObj);
 
                         if (i == oMeta.day && monthIndex == oMeta.thisMonth) {
-                            padding += "<td class='currentmonth currentday " + classHasEvents + " " + classHasPendingEvents + "'>" + i + "</td>";
+                            padding += "<td data-index='" + i + "' ng-click='goToEvents(" + actualIndex + ")' class='currentmonth currentday " + classHasEvents + " " + classHasPendingEvents + "'>" + i + "</td>";
                         } else {
-                            padding += "<td class='currentmonth " + classHasEvents + " " + classHasPendingEvents + "'>" + i + "</td>";
+                            padding += "<td data-index='" + i + "' ng-click='goToEvents(" + actualIndex + ")' class='currentmonth " + classHasEvents + " " + classHasPendingEvents + "'>" + i + "</td>";
                         }
 
                         firstWeekDay++;
@@ -173,12 +305,9 @@
                     ctable += padding;
                     ctable += "</tr></table>";
 
-                    console.log(oMeta);
-
                     return {meta: oMeta, html: ctable};
                 }
             }
         }
-    }])
-
+    }]);
 })(angular);
